330fcc6fc0a23d3336be882271f885bd352867fe
Farhan50s <mf0224530@gmail.com>
2025-06-30 15:15:19 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Zen.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Zen(db.Model):
+    No = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date = db.Column(db.DateTime, default = datetime.utcnow())
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Zen(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    Todo = Zen.query.all()
+    return render_template('index.html', Todo=Todo)
+
+@app.route('/update/<int:no>', methods=['GET', 'POST'])
+def update():
+    
+    return redirect('/')
+
+@app.route('/delete/<int:no>')
+def delete(no):
+    todo = Zen.query.filter_by(No=no).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
+
+
+@app.route('/about')
+def about():
+    return render_template ('about.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all() # Create the database tables if they don't exist
+    app.run(debug=True)
